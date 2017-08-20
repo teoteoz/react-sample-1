aeec806000a6b424b42fb52bb5c36b41f51c5315
Matteo Casu <matteo.casu91@gmail.com>
2017-08-20 19:37:54 +0200

    props

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,41 @@
-console.log("It works");
+import React from 'react';
+import { render } from "react-dom";
+
+import { Header } from "./components/Header";
+import { Home } from "./components/Home"; 
+
+class App extends React.Component {
+    
+    render () {
+
+        var user = {
+            name: "Anna",
+            hobbies: ["Sports", "Reading"]
+        }
+
+        return (
+            <div className="container">
+
+                <div className="row">
+                    <div className="col-xs-10 col-xs-offset-1">
+                        <Header />
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col-xs-10 col-xs-offset-1">
+                        <Home name={"Max"} age={26} user={user}>
+                            <p>This is a paragraph!</p>
+                        </Home>
+                    </div>
+                </div>
+
+            </div>
+        );
+    }
+}
+
+var app = document.getElementById("app");
+render(<App />, app);
+
+
